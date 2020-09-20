ae56d17b33817e484743aafa071425c55561102b
Rocio Batres <jonasgirl21@gmail.com>
2020-09-20 21:15:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,30 +7,8 @@ class Dog
     self.save  
   end 
   
-  def save
-    @@all << self 
-  end 
-  
-  def self.all  
-    @all 
-  end 
-  
-  def name 
-    @name 
-  end 
-  
   def self.all 
-    @@all
-  end 
-  
-  def self.print_all
-    @@all.each do |dog| 
-      puts dog.name 
-  end 
-  
-  def self.clear_all 
-    @@all = []  
-  end 
+    
   
   
 end   
