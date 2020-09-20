acbff23f540b5185192b5be05f3ce7c211d6d9f5
Rocio Batres <jonasgirl21@gmail.com>
2020-09-20 20:41:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    self.save  
+  end 
+  
+  def save
+    @save= save 
+    save = @@all << self 
+  end 
+  
+  def save
+    @save
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.print_all
+    puts @@all    
+  end 
+  
+  def self.clear_all 
+    @@all.clear 
+  end 
+  
+  
+end   
